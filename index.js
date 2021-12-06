ea9a982e00b982f2f066dd63987dffcf6844c65c
= <rtyenriques@gmail.com>
2021-12-06 14:18:43 -0800

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
     } else if (e.key === "ArrowRight") {
-      moveDodgerLeft();
+      moveDodgerRight();
+    } else {e.key === "ArrowUp"} {
+      moveDodgerUp();
     }
   });
   
@@ -22,4 +24,13 @@ document.addEventListener("keydown", function(e) {
     if (left > 0) {
       dodger.style.left = `${left + 1}px`;
     }
+  }
+
+  function moveDodgerUp() {
+    let upNumbers = dodger.style.bottom.replace("px", "");
+    let up = parseInt(upNumbers, 10);
+
+  if (up > 0) {
+    dodger.style.bottom = `${up + 1}px`;
+  }
   }
